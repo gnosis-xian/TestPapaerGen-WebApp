86814f9a37db179567ed665acde6199ebc372104
gnosis <gaojing1996@vip.qq.com>
2023-08-31 23:04:48 +0800

    change port

--- cd/startup.sh
+++ cd/startup.sh
@@ -1,5 +1,5 @@
 nohup \
 /root/openlogic-openjdk-8u372-b07-linux-x64/bin/java \
 -jar /root/TestPapaerGen-WebApp/TestPapaerGen-Backend/target/test-paper-generation-3.0.0.jar \
---server.port=8081 \
+--server.port=18081 \
 > /root/TestPapaerGen-WebApp/log/app.log 2>&1 &
